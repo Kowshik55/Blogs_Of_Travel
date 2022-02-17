15eec39af293afc23fcbcdad38f22060a7e18a3d
Kowshik55 <89611346+Kowshik55@users.noreply.github.com>
2022-02-16 21:53:22 -0600

    Update settings.py

--- travelblog/settings.py
+++ travelblog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-65xp+&_6y4p!mde$ctdn1urarv*(5_np0a+axx+=oh6%hgr9t3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
